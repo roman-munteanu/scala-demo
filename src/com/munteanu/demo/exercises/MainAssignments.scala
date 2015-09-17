286cf96c1f0fd8588cf14a052a99088156d03b30
Roman Munteanu <Roman.Munteanu@endava.com>
2015-09-17 18:54:10 +0300

    coins

--- src/com/munteanu/demo/exercises/MainAssignments.scala
+++ src/com/munteanu/demo/exercises/MainAssignments.scala
@@ -102,9 +102,9 @@ object MainAssignments extends App {
       processList(processed)
   }
 
-  println(processList("())(()".toList).mkString(" "))
-  println(processList("(()())".toList).mkString(" "))
-  println(processList("(((()".toList).mkString(" "))
+//  println(processList("())(()".toList).mkString(" "))
+//  println(processList("(()())".toList).mkString(" "))
+//  println(processList("(((()".toList).mkString(" "))
 
 
 //  println(balance("(if (zero? x) max (/ 1 x))".toList))
@@ -112,5 +112,51 @@ object MainAssignments extends App {
 //  println(balance(":-)".toList))
 //  println(balance("())(".toList))
 
+
+  // assert(countChange(300,List(5,10,20,50,100,200,500)) === 1022)
+
+
+//  def countChange(money: Int, coins: List[Int]): Int = {
+//
+//  }
+
+//  def countChange(money: Int, coins: List[Int]): Int =
+//    if (money == 0) 1
+//    else if (coins.isEmpty || money < 0) 0
+//    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+  def countChange(money: Int, coins: List[Int]): Int = money match {
+    case 0 => 1
+    case m if m < 0 || coins.isEmpty => 0
+    case _ => countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+
+
+//  println(countChange(300,List(5,10,20,50,100,200,500)))
+//  println(countChange(301,List(5,10,20,50,100,200,500)))
+//  println(countChange(300,List(500,5,50,100,20,200,10)))
+
+
+  // 4: [1,2]
+//  var count = 0
+//  for (i <- 0 to 4/1)
+//    for (j <- 0 to 4/2)
+//      if (1*i + 2*j - 4 == 0) count += 1
+//  println(count) // 3
+
+  // 8: [1,2,6]
+  var count = 0
+  for (i <- 0 to 8/1)
+    for (j <- 0 to 8/2)
+      for (k <- 0 to 8/6)
+        if (1*i + 2*j + 6*k - 8 == 0) count += 1
+
+//  println(count) // 7
+
+  def func(args: Int*): Unit = {
+    for (i <- 0 until args.length)
+      println(args(i))
+  }
+  func(1,2,6)
 
 }
